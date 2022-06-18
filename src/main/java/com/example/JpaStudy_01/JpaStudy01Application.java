c31ecff80cbb0f7a7e478f7e8bb5e628fcb0fe61
Topy <gmsdl1994@naver.com>
2022-06-19 02:39:14 +0900

    first commit

---
+++ src/main/java/com/example/JpaStudy_01/JpaStudy01Application.java
@@ -0,0 +1,13 @@
+package com.example.JpaStudy_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaStudy01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaStudy01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JpaStudy_01/JpaStudy01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JpaStudy_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaStudy01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
