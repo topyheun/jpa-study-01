1a3e31fbd149e8382111216a182e19d863af8788
Topy <gmsdl1994@naver.com>
2022-06-22 01:18:39 +0900

    [feat] H2연동 및 JPA 테스트
    강의 시작 전 H2 database설치 및 연동과 JPA테스트 진행

---
+++ src/main/java/com/example/JpaStudy_01/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.JpaStudy_01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("data", "슬래쉬 있는 거");
+        return "hello";
+    }
+}
--- src/test/java/com/example/JpaStudy_01/JpaStudy01ApplicationTests.java
+++ src/test/java/com/example/JpaStudy_01/JpaStudy01ApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.JpaStudy_01;
 
+import com.example.JpaStudy_01.domain.Member;
+import com.example.JpaStudy_01.domain.MemberRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 class JpaStudy01ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
